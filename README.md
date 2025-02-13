9361af47917dd59efb8a74c39e809a246290dd03
abhaydmathur <admathur@iitk.ac.in>
2025-02-13 18:24:33 +0100

    data utils

--- README.md
+++ README.md
@@ -5,24 +5,24 @@ Course Project for CSC_5IA23_TA - Deep Learning Based Computer Vision
 
 This project focuses on fire detection using a provided dataset. The dataset contains three folders: a training set, a validation set, and a test set. However, there are specific constraints and guidelines to follow:
 
-## Dataset Access and Restrictions:
+### Dataset Access and Restrictions:
 
 Please download the dataset from : https://www.kaggle.com/datasets/abdelghaniaaba/wildfire-prediction-dataset/code
 the dataset is composed of a training set, a validation set, and a test set.
 You are not allowed to access the label of the training set.
 Important: Any use of annotations from the training set will result in a zero grade.
 
-## Dataset Splitting:
+### Dataset Splitting:
 
 Split the original validation set into a new validation set and a new train set.
 You can use the original training set creatively, but without utilizing its labels.
 
-## Model Training:
+### Model Training:
 
 Train your deep neural network (DNN) using the newly defined training and validation sets.
 Feel free to use any methodology or additional resources to improve your model's performance, as long as you respect the annotation constraints.
 
-## Evaluation and Submission:
+### Evaluation and Submission:
 
 a. Evaluate your model's performance on the new test set.
 b. Submit the following:
@@ -30,3 +30,7 @@ b. Submit the following:
 Code: Implemented in PyTorch.
 Pretrained Model: Include the trained weights.
 Report: A concise explanation of your approach, the methods used, and the reasoning behind your choices.
+
+## Ideas
+
+* **SimCLR?**
--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -1,23 +1,64 @@
 import glob
 from torch.utils.data import Dataset
 from PIL import Image
+import pandas as pd
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+def show_image(image):
+    plt.imshow(image.permute(1, 2, 0))
+    plt.axis('off')
+    plt.show()
 
 class WildfireDataset(Dataset):
     def __init__(
             self,
             root_dir,
             split='train',
+            labeled=False,
     ):
-        pass
+        self.root_dir = root_dir
+        self.split = split
+        if not labeled:
+            self.split = split + '_unlabeled'
+
+        self.labeled = labeled
+
+        meta_file = f'{root_dir}/{split}.csv'
+        self.meta = pd.read_csv(meta_file)
+        self.meta = self.meta.dropna().drop_duplicates()
+
+        ids = np.arange(len(self.meta))
+        self.id_to_filename = dict(zip(ids, self.meta['filename'].values))
+        if self.labeled:
+            self.id_to_label = dict(zip(ids, self.meta['label'].values))
+        self.id_to_coordx = dict(zip(ids, self.meta['coord_x'].values))
+        self.id_to_coordy = dict(zip(ids, self.meta['coord_y'].values))
 
     def load_image(self, path):
         image = Image.open(path).convert('RGB')
-        return np.array(image)
+        image = np.array(image, dtype=np.float32) / 255.0
+        return torch.tensor(image).permute(2, 0, 1)
 
     def __len__(self):
-        pass    
+        return len(self.meta)
 
-    def __getitem__(self, index):
-        pass
+    def __getitem__(self, idx):
+        filename = self.id_to_filename[idx]
+        image = self.load_image(filename)
+        coords = torch.tensor([self.id_to_coordx[idx], self.id_to_coordy[idx]], dtype=torch.float32)
+
+        if self.labeled:
+            label = 1 if self.id_to_label[idx] == 'wildfire' else 0
+            return {
+                'image': image,
+                'label': torch.tensor(label, dtype=torch.long),
+                'coords': coords,
+            }
+        else:
+            return {
+                'image': image,
+                'coords': coords,
+            }
 
